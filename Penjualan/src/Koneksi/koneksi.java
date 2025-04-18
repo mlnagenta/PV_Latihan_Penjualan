35afdd181cde9b733f5a5859c18857378481cffc
Maulana Genta Laksono <maulanagnta@gmail.com>
2025-04-18 15:32:43 +0700

    Add files via upload

---
+++ Penjualan/src/Koneksi/koneksi.java
@@ -0,0 +1,27 @@
+package Koneksi;
+import java.sql.*;
+/**
+ *
+ * @author Genta
+ */
+public class koneksi {
+    private Connection koneksi;
+    public Connection connect(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("berhasil konek");
+        }
+        catch(ClassNotFoundException ex){
+            System.out.println("gagal koneksi"+ex);
+        }
+        String url = "jdbc:mysql://localhost/penjualan";
+        try{
+            koneksi = DriverManager.getConnection(url,"root","");
+            System.out.println("berhasil koneksi database");
+        }
+        catch (SQLException ex){
+            System.out.println("gagal koneksi database"+ex);
+        }
+        return koneksi;
+    }
+}
